31a070d52e3a7e28f26e293d7b0da01f7bb254c3
pavel.lezin <pavellezin@gmail.com>
2022-01-18 18:46:49 +0500

    1_01_user_with_lombok.patch

---
+++ src/main/java/pro/paullezin/bootjava/vouting/model/Role.java
@@ -0,0 +1,6 @@
+package pro.paullezin.bootjava.vouting.model;
+
+public enum Role {
+    ROLE_USER,
+    ROLE_ADMIN
+}
---
+++ src/main/java/pro/paullezin/bootjava/vouting/model/User.java
@@ -0,0 +1,23 @@
+package pro.paullezin.bootjava.vouting.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    private String email;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String password;
+
+    private Set<Role> roles;
+}
