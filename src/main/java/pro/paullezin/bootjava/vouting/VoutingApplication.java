075984f318e655e7dbe2ea3c8583a4e1e998307a
pavel.lezin <pavellezin@gmail.com>
2022-01-18 12:37:52 +0500

    initial commit

---
+++ src/main/java/pro/paullezin/bootjava/vouting/VoutingApplication.java
@@ -0,0 +1,13 @@
+package pro.paullezin.bootjava.vouting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoutingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VoutingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/paullezin/bootjava/vouting/VoutingApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.paullezin.bootjava.vouting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoutingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
