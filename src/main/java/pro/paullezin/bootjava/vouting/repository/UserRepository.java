dd2e10d9e853f5c612f3ba31b9faeb62a45357f0
pavel.lezin <pavellezin@gmail.com>
2022-01-23 17:55:23 +0500

    3_01_jpa_data_rest.patch

--- src/main/java/pro/paullezin/bootjava/vouting/repository/UserRepository.java
+++ src/main/java/pro/paullezin/bootjava/vouting/repository/UserRepository.java
@@ -2,6 +2,7 @@ package pro.paullezin.bootjava.vouting.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.transaction.annotation.Transactional;
 import pro.paullezin.bootjava.vouting.model.User;
 
@@ -12,8 +13,10 @@ import java.util.Optional;
 @Transactional(readOnly = true)
 public interface UserRepository extends JpaRepository<User, Integer> {
 
+    @RestResource(rel = "by-email", path = "by-email")
     @Query("SELECT u FROM User u WHERE u.email = LOWER(:email) ")
     Optional<User> findByEmailIgnoreCase(String email);
 
+    @RestResource(rel = "by-lastname", path = "by-lastname")
     List<User> findByLastNameContainingIgnoreCase(String lastName);
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
 INSERT INTO users (EMAIL, FIRST_NAME, LAST_NAME, PASSWORD)
 VALUES ('user@gmail.com', 'User_First', 'User_Last', 'user'),
-       ('admin@gmail.ru', 'Admin_First', 'Admin_Last', 'admin');
+       ('admin@gmail.com', 'Admin_First', 'Admin_Last', 'admin');
 
 INSERT INTO USER_ROLE (ROLE, USER_ID)
 VALUES ('ROLE_USER', 1),
